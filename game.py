f4853fbdb93a4d0291213e986eac299afddc4b2b
Teky1 <officialteky@gmail.com>
2021-04-23 00:28:13 -0400

    switched to 2d array

--- game.py
+++ game.py
@@ -1,20 +1,25 @@
 
+# NOTE: ALL LOCATIONS ARE IN y, x
+# because i keep forgetting
 
-
-def kingMoves(board, isBlack, index):
+def kingMoves(board, location):
     '''
     get all legal moves for a king piece
 
-    board: Board object
-    isBlack: boolean whether the target piece is black
-    index: index of the target piece
+    board: Board array
+    location: index of the target piece
     '''
+
+    is_black = board[location[0]][location[1]].isupper()
     available_moves = []
-    moves_to_check = [-11, -10, -9, -1, 1, 9, 10, 11]
+    moves_to_check = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
     for move in moves_to_check:
-        target = board.board[index + move]
-        if target != "0" and (target == " " or target.isupper() == isBlack):
-            available_moves.append(index + move)
+        try:
+            target = board[location[0] + move[0]][location[1] + move[1]]
+            if target == " " or target.isupper() != is_black:
+                available_moves.append([location[0]+move[0], location[1]+move[1]])
+        except IndexError:
+            pass
     return available_moves
 
 
@@ -39,22 +44,16 @@ class Board:
         r - rook
         p - pawn
         - uppercase letters represent black pieces and lower case represent white pieces
-        - 0's represent out of bounds spaces so we can easily identify moves that go out of bounds
-        by seeing whether they land on a 0 (this is to avoid having to deal with index errors and stuff)
         '''
-        self.board = ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
-                      '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
-                      '0', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R', '0',
-                      '0', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', '0',
-                      '0', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0',
-                      '0', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0',
-                      '0', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0',
-                      '0', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0',
-                      '0', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', '0',
-                      '0', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r', '0',
-                      '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
-                      '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
-                      ]
+        self.board = [["R", "N", "B", "Q", "K", "B", "N", "R"],
+                      ["P", "P", "P", "P", "P", "P", "P", "P"],
+                      [" ", " ", " ", " ", " ", " ", " ", " "],
+                      [" ", " ", " ", " ", " ", " ", " ", " "],
+                      [" ", " ", " ", " ", " ", " ", " ", " "],
+                      [" ", " ", " ", " ", " ", " ", " ", " "],
+                      ["p", "p", "p", "p", "p", "p", "p", "p"],
+                      ["r", "n", "b", "q", "k", "b", "n", "r"],]
+
         # turnNum increments every time a move is made
         # if it's odd its whites turn if its even its blacks turn
         self.turnNum = 1
@@ -75,12 +74,12 @@ class Board:
         # process the raw input here
 
 
-    def makeMove(self, piece_index, target_index, special=None):
+    def makeMove(self, piece_location, target_location, special=None):
         # Moves a piece on the board from one position to the other
         # Should only be called after the move is confirmed to be valid
         if special is None:
-            self.board[target_index] = self.board[piece_index]
-            self.board[piece_index] = ' '
+            self.board[target_location[0]][target_location[1]] = self.board[piece_location[0]][piece_location[1]]
+            self.board[piece_location[0]][piece_location[1]] = ' '
         elif special == "castle":
             # add code to handle castling here
             pass
